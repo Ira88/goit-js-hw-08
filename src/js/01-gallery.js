53bc9e5382d1bb8003557381af2bfc0f0c367ae7
Ira88 <111458593+Ira88@users.noreply.github.com>
2023-02-28 13:56:58 +0200

    task 1

--- src/js/01-gallery.js
+++ src/js/01-gallery.js
@@ -1,5 +1,24 @@
+// Описаний в документації
+import SimpleLightbox from "simplelightbox";
+// Додатковий імпорт стилів
+import "simplelightbox/dist/simple-lightbox.min.css";
 // Add imports above this line
 import { galleryItems } from './gallery-items';
 // Change code below this line
-
+const gallery = document.querySelector(".gallery");
+const usage = galleryItems.map(({preview, original, description}) => {
+return `<div class="gallery__item">
+<a class="gallery__item" href="${original}">
+  <img class="gallery__image" src="${preview}" 
+  alt="${description}" />
+</a>
+</div>`
+}).join('');
+gallery.insertAdjacentHTML("beforeend", usage);
+new SimpleLightbox('.gallery a', {
+    captionsData: 'alt',
+    captionDelay: 250,
+    captionPosition: 'bottom',
+    
+});
 console.log(galleryItems);
